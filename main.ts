6b3697bcfbe35fa4d509fb4065d2b50966472d5a
Jure10 <60433116+Jure10@users.noreply.github.com>
2020-01-31 19:58:31 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
-motors.largeBC.tank(50, 50, 2.45, MoveUnit.Rotations) 
+motors.largeBC.tank(50, 50, 2, MoveUnit.Rotations)
+motors.largeA.run(50, 1, MoveUnit.Rotations)
+motors.largeBC.tank(50, 50, 5, MoveUnit.Rotations)
