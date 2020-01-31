977768299dfb67792f1c392f382e38dd48475deb
Jure10 <60433116+Jure10@users.noreply.github.com>
2020-01-31 20:32:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-for (let i = 0; i < 4; i++) {
+for (let index = 0; index < 4; index++) {
     motors.largeBC.tank(50, 50, 2, MoveUnit.Rotations)
     motors.largeA.run(50, 1, MoveUnit.Rotations)
     motors.largeBC.tank(50, 50, 5, MoveUnit.Rotations)
     motors.stopAll()
 }
-for (let i = 0; i < 4; i++) {
+for (let index = 0; index < 2; index++) {
     motors.largeBC.tank(50, 50, 2, MoveUnit.Rotations)
     motors.largeA.run(50, 1, MoveUnit.Rotations)
-    motors.largeBC.tank(30, 30, 5, MoveUnit.Rotations)
+    motors.largeBC.tank(30, 30, 15, MoveUnit.Seconds)
 }
